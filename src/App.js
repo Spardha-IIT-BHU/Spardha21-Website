9eff67cc9e2b07dddc28e4970cd45d0ae80a53af
Ayush gangwani <gangwaniah@gmail.com>
2022-03-02 21:39:21 +0530

    Added Navbar, Favicon, Title and Meta Tags

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route  } from "react-router-dom";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import About from "./components/About/About";
 import Events from "./components/Events/Events";
+import Header from "./components/Header/Header";
 
 function App() {
   return (
     <>
-      <div>
-        Top Slider
-      </div>
       <Router>
+        <div>
+          <Header/>
+        </div>
         <Routes>
           <Route path="/" element={<About />} />
           <Route path="/about" element={<About />} />
           <Route path="/events" element={<Events />} />
         </Routes>
       </Router>
-      <div>
-        Contact Us
-      </div>
+      <div>Contact Us</div>
     </>
   );
 }
