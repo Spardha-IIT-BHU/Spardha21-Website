876a4586c969782cb7f3de3d2496487ae61dc078
SanidhyaTaparia <tapariasanidhya@gmail.com>
2022-03-02 15:17:36 +0530

    Added basic routing and structuring

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route  } from "react-router-dom";
+import About from "./components/About/About";
+import Events from "./components/Events/Events";
+
 function App() {
   return (
-    <div>
-      welcome to spardha website.
-    </div>
+    <>
+      <div>
+        Top Slider
+      </div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<About />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/events" element={<Events />} />
+        </Routes>
+      </Router>
+      <div>
+        Contact Us
+      </div>
+    </>
   );
 }
 
