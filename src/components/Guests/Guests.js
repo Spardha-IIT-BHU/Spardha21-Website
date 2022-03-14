5aa551661f85765b3b8c3c139284f9c3b5834872
SanidhyaTaparia <tapariasanidhya@gmail.com>
2022-03-14 19:14:36 +0530

    Added basic layout of guests

---
+++ src/components/Guests/Guests.js
@@ -0,0 +1,169 @@
+import React from 'react'
+import "./Guests.css"
+
+const Guests = () => {
+  return (
+    <>
+    <div id="guests-video">
+    <section id="guests" className="ftco-section_guests" style={{paddingBottom: '0'}}>
+        <div className="container-fluid_guests">
+            <div className="row_guests justify-content-center_guests mb-5">
+                <div className="col-md-7 heading-section_guests text-center ftco-animate">
+                    <h3 className="subheading_guests" style={{ color: '#6db549', fontWeight: '900' }}>Patrons of Spardha</h3>
+                    <h2 className="mb-1" style={{color: "white",fontWeight: '900'}}>Guests</h2>
+                </div>
+            </div>
+            <div className="row_guests">
+                 <div className="col-md-3 col-sm-3 col-6">
+                    <img src="/images/guests/18.png" className="img-fluid_guests img-guests" alt="guest_image_alt"/>
+                    <div className="middle_guests">
+                        <div className="guest-text">Sushil&nbsp;Kumar</div>
+                      </div>
+                </div>
+                <div className="col-md-3 col-sm-3 col-6">
+                    <img src="/images/guests/6.png" className="img-fluid_guests img-guests" alt="guest_image_alt"/>
+                    <div className="middle_guests">
+                        <div className="guest-text">Dhanraj&nbsp;Pillay</div>
+                      </div>
+                </div>
+                <div className="col-md-3 col-sm-3 col-6">
+                    <img src="/images/guests/16.png" className="img-fluid_guests img-guests" alt="guest_image_alt"/>
+                    <div className="middle_guests">
+                        <div className="guest-text">Sardar&nbsp;Singh</div>
+                      </div>
+                </div>
+                <div className="col-md-3 col-sm-3 col-6">
+                    <img src="/images/guests/14.png" className="img-fluid_guests img-guests" alt="guest_image_alt"/>
+                    <div className="middle_guests">
+                        <div className="guest-text">Sakshi&nbsp;Malik</div>
+                      </div>
+                </div>
+                <div className="col-md-3 col-sm-3 col-6">
+                    <img src="/images/guests/23.png" className="img-fluid_guests img-guests" alt="guest_image_alt"/>
+                    <div className="middle_guests">
+                        <div className="guest-text">VVS&nbsp;Laxman</div>
+                      </div>
+                </div>
+                <div className="col-md-3 col-sm-3 col-6">
+                    <img src="/images/guests/12.png" className="img-fluid_guests img-guests" alt="guest_image_alt"/>
+                    <div className="middle_guests">
+                        <div className="guest-text">R.S.&nbsp;Rathore</div>
+                      </div>
+                </div>
+                <div className="col-md-3 col-sm-3 col-6">
+                    <img src="/images/guests/5.png" className="img-fluid_guests img-guests" alt="guest_image_alt"/>
+                    <div className="middle_guests">
+                        <div className="guest-text">Deepa&nbsp;Malik</div>
+                      </div>
+                </div>
+                <div className="col-md-3 col-sm-3 col-6">
+                    <img src="/images/guests/24.png" className="img-fluid_guests img-guests" alt="guest_image_alt"/>
+                    <div className="middle_guests">
+                        <div className="guest-text">Yogeshwar&nbsp;Dutt</div>
+                      </div>
+                </div>
+                <div className="col-md-3 col-sm-3 col-6">
+                    <img src="/images/guests/4.png" className="img-fluid_guests img-guests" alt="guest_image_alt"/>
+                    <div className="middle_guests">
+                        <div className="guest-text">Balbir&nbsp;Singh</div>
+                      </div>
+                </div>
+                <div className="col-md-3 col-sm-3 col-6">
+                    <img src="/images/guests/21.png" className="img-fluid_guests img-guests" alt="guest_image_alt"/>
+                    <div className="middle_guests">
+                        <div className="guest-text">Tanvie&nbsp;Hans</div>
+                      </div>
+                </div>
+                <div className="col-md-3 col-sm-3 col-6">
+                    <img src="/images/guests/1.png" className="img-fluid_guests img-guests" alt="guest_image_alt"/>
+                    <div className="middle_guests">
+                        <div className="guest-text">Akhil&nbsp;Kumar</div>
+                      </div>
+                </div>
+                <div className="col-md-3 col-sm-3 col-6">
+                    <img src="/images/guests/15.png" className="img-fluid_guests img-guests" alt="guest_image_alt"/>
+                    <div className="middle_guests">
+                        <div className="guest-text">Sandeep&nbsp;Singh</div>
+                      </div>
+                </div>
+                <div className="col-md-3 col-sm-3 col-6">
+                    <img src="/images/guests/10.png" className="img-fluid_guests img-guests" alt="guest_image_alt"/>
+                    <div className="middle_guests">
+                        <div className="guest-text">Prashanti&nbsp;Singh</div>
+                      </div>
+                </div>
+                <div className="col-md-3 col-sm-3 col-6">
+                    <img src="/images/guests/13.png" className="img-fluid_guests img-guests" alt="guest_image_alt"/>
+                    <div className="middle_guests">
+                        <div className="guest-text">Sajjan&nbsp;Singh&nbsp;Cheema</div>
+                      </div>
+                </div>
+                <div className="col-md-3 col-sm-3 col-6">
+                    <img src="/images/guests/9.png" className="img-fluid_guests img-guests" alt="guest_image_alt"/>
+                    <div className="middle_guests">
+                        <div className="guest-text">Prachi&nbsp;Tehlan</div>
+                      </div>
+                </div>
+                <div className="col-md-3 col-sm-3 col-6">
+                    <img src="/images/guests/17.png" className="img-fluid_guests img-guests" alt="guest_image_alt"/>
+                    <div className="middle_guests">
+                        <div className="guest-text">Shikha&nbsp;Pandey</div>
+                      </div>
+                </div>
+                <div className="col-md-3 col-sm-3 col-6">
+                    <img src="/images/guests/11.png" className="img-fluid_guests img-guests" alt="guest_image_alt"/>
+                    <div className="middle_guests">
+                        <div className="guest-text">Praveen&nbsp;Thipsay</div>
+                      </div>
+                </div>
+                <div className="col-md-3 col-sm-3 col-6">
+                    <img src="/images/guests/8.png" className="img-fluid_guests img-guests" alt="guest_image_alt"/>
+                    <div className="middle_guests">
+                        <div className="guest-text">Kunjarani&nbsp;Devi</div>
+                      </div>
+                </div>
+                <div className="col-md-3 col-sm-3 col-6">
+                    <img src="/images/guests/19.png" className="img-fluid_guests img-guests" alt="guest_image_alt"/>
+                    <div className="middle_guests">
+                        <div className="guest-text">Sushma&nbsp;Verma</div>
+                      </div>
+                </div>
+                <div className="col-md-3 col-sm-3 col-6">
+                    <img src="/images/guests/2.png" className="img-fluid_guests img-guests" alt="guest_image_alt"/>
+                    <div className="middle_guests">
+                        <div className="guest-text">Amit&nbsp;Panghal</div>
+                      </div>
+                </div>
+                <div className="col-md-3 col-sm-3 col-6">
+                    <img src="/images/guests/3.png" className="img-fluid_guests img-guests" alt="guest_image_alt"/>
+                    <div className="middle_guests">
+                        <div className="guest-text">Anjum&nbsp;Chopra</div>
+                      </div>
+                </div>
+                <div className="col-md-3 col-sm-3 col-6">
+                    <img src="/images/guests/20.png" className="img-fluid_guests img-guests" alt="guest_image_alt"/>
+                    <div className="middle_guests">
+                        <div className="guest-text">Tania&nbsp;Sachdev</div>
+                      </div>
+                </div>
+                <div className="col-md-3 col-sm-3 col-6">
+                    <img src="/images/guests/7.png" className="img-fluid_guests img-guests" alt="guest_image_alt"/>
+                    <div className="middle_guests">
+                        <div className="guest-text">Dilip&nbsp;Tirkey</div>
+                      </div>
+                </div>
+                <div className="col-md-3 col-sm-3 col-6">
+                    <img src="/images/guests/22.png" className="img-fluid_guests img-guests" alt="guest_image_alt"/>
+                    <div className="middle_guests">
+                        <div className="guest-text">Varun&nbsp;Singh&nbsp;Bhati</div>
+                      </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    </div>
+    </>
+  )
+}
+
+export default Guests
